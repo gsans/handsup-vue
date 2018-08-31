95dfe5d46c75ea7cae301ed2b5226450e4a8e6a2
Gerard Sans <gerard.sans@gmail.com>
2018-08-31 00:33:03 -0300

    Added scrollTo and mutation

--- src/components/AddQuestion.vue
+++ src/components/AddQuestion.vue
@@ -1,21 +1,27 @@
 <template>
   <div class='bottom_wrapper clearfix'>
     <div class='message_input_wrapper'>
-      <form>
-      <input ref="input"
+      <form v-on:submit.prevent="submit">
+      <input
         class='message_input'
         placeholder='Type your question here...'
         :maxlength="max" v-model="text"
+        @keyup="keyup"
       />
       <div class='counter'>{{max - text.length}}/{{max}}</div>
-      <button class='send_message' type="button" :disabled="text.length===0"
-        @click="submit">Send</button>
+      <button class='send_message' type="submit"
+        :disabled="text.length===0">Send</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import { 
+  ListQuestions as QUESTIONS, 
+  AddQuestion as ADD_QUESTION
+} from '../appsync/graphql'
+var VueScrollTo = require('vue-scrollto');
 const MAX_CHAR = 140
 
 export default {
@@ -24,22 +30,65 @@ export default {
     max: MAX_CHAR
   }),
   methods: {
+    keyup(event) {
+      this.text = event.target.value;
+    },
     submit(event){
       event.preventDefault()
       if (!this.text || this.text.length === 0) {
         return;
       }
-      this.$emit("add", { body: this.$refs.input.value });
-      this.$refs.input.value=''; 
-      /* 
-        .addQuestion(this.input.value, this.props.auth.userId)
-        .then(() => {
-          Alert.info('Question sent! ✨🚀', ALERT_DEFAULT)
-          this.input.value = ''
-          this.setState({
-            chars_left: MAX_CHAR,
-          })
-        }) */      
+      //this.$emit("add", { body: this.text });
+      const question = { body: this.text };
+      this.$apollo.mutate({
+        mutation: ADD_QUESTION,
+        variables: question,
+        update: (store, { data: { createQuestion } }) => {
+          // called few times without updates
+          if (createQuestion && 'id' in createQuestion) {
+            const data = store.readQuery({ query: QUESTIONS })
+            data.questions.items.push(createQuestion)
+            store.writeQuery({ query: QUESTIONS, data })
+          }
+        },
+        /* optimisticResponse: {
+          __typename: 'Mutation',
+          createQuestion: {
+            __typename: 'Question',
+            ...question
+          }
+        }, */
+      })
+      .then(() => {
+        this.$notify({
+          type: 'success',
+          group: 'alerts',
+          title: 'Success',
+          text: 'Question added! ✨🚀',
+        });
+
+        this.text = ""; 
+        setTimeout(() => {
+          var options = {
+            container: '#app',
+            easing: 'ease-in-out',
+            offset: 0,
+            cancelable: true,
+            x: false,
+            y: true
+          }
+          let elem = document.querySelector('#bottom');
+          VueScrollTo.scrollTo(elem, 500, options);
+        }, 500)
+      })
+      .catch(error => {
+        this.$notify({
+          type: 'error',
+          group: 'alerts',
+          title: 'Error',
+          text: error,
+        });
+      })      
     }
   }
 };
